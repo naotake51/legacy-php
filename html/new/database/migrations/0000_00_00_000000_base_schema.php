e682abed4e5d6a9777f68b1c2408b148744e01c9
n-takeshita <naokisyuu@gmail.com>
2025-05-12 19:34:57 +0900

    Migrationでbase_schema.sqlからベースとなるスキーマを生成。デフォルトで用意されたユーザーテーブルまわりのマイグレーションファイルを削除

---
+++ html/new/database/migrations/0000_00_00_000000_base_schema.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        /**
+         * マイグレーションをLaravelで管理する以前のスキーマファイル。ローカル環境以外で実行してはいけない。
+         */
+        if (config("app.env") !== "local") {
+            return;
+        }
+
+        $sqlFile = base_path('database/base_schema.sql');
+        if (!file_exists($sqlFile)) {
+            throw new \Exception("SQL file not found: $sqlFile");
+        }
+
+        $sql = file_get_contents($sqlFile);
+        DB::unprepared($sql);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
